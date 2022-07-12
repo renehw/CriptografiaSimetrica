1280c70ec1be95f1bb81619aca9e5450a570d29b
andre voronhuk <andrevoronhuk@gmail.com>
2022-07-12 20:38:49 -0300

    pulado linhas no inicio dos prints

--- criptografar_dados.py
+++ criptografar_dados.py
@@ -4,10 +4,13 @@ class CriptografarDados:
     file = open('chave.key', 'rb')
     chave = file.read()
     file.close
+    print("\n\n")
     print(chave)
 
     dados = 'Este é um texto criptografado simetricamente'
     f = Fernet(chave)
 
     dadosCriptografados = f.encrypt(b"Este e um texto criptografado simetricamente")
-    print(f'{dadosCriptografados}') 
+    print(dadosCriptografados)
+    
+    #print(f'{dadosCriptografados}') 
