81dc547591f354af0cd3351c681430ddb43d2ef6
andre voronhuk <andrevoronhuk@gmail.com>
2022-07-12 22:05:53 -0300

    adicionado funcao de cifrar e decifrar em AES

--- criptografar_dados_AES.py
+++ criptografar_dados_AES.py
@@ -1,11 +1,19 @@
 import os
 from cryptography.hazmat.primitives.ciphers import Cipher, algorithms, modes
-key = os.urandom(32)
-iv = os.urandom(16)
-cipher = Cipher(algorithms.AES(key), modes.CBC(iv))
-encryptor = cipher.encryptor()
-ct = encryptor.update(b"a secret message") + encryptor.finalize()
-print(ct)
-decryptor = cipher.decryptor()
-decryp = decryptor.update(ct) + decryptor.finalize()
-print(decryp)
+
+
+class Cifrador:
+    def __init__(self):
+        pass
+    
+    
+    def criptografar(self,key):
+      
+       # key = os.urandom(32)  #substituir por chave lida do arquivo       
+        iv = os.urandom(16)   #gera numero aleatorio de 16 bytes
+        cipher = Cipher(algorithms.AES(key), modes.CBC(b"\x00" * 16))
+        encryptor = cipher.encryptor()
+        ct = encryptor.update(b"a secret message") + encryptor.finalize()
+        return ct
+    
+    
